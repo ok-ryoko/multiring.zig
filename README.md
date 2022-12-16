cef76f4770c5d1fff42807e3aea2af71e69e51fb
OK Ryoko <ryoko@kyomu.jp.net>
2022-12-15 23:00:06 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# multiring.zig
+
+The *multiring* is a singly linked, cyclic and hierarchical [abstract data type] (ADT). This pointer-based [Zig] implementation is intended to expose a rich set of methods that preserve the structural integrity of the multiring.
+
+## Concepts
+
+<picture>
+  <source
+    media="(prefers-color-scheme: dark)"
+    srcset="./docs/img/multiring-github-dark.png"
+  >
+  <source
+    media="(prefers-color-scheme: light)"
+    srcset="./docs/img/multiring-github-light.png"
+  >
+  <img
+    title="multiring"
+    img alt=""
+    src="./docs/img/multiring-github-light.png"
+    align="right"
+    height="120"
+  >
+</picture>
+
+A **singly [linked list]** is a sequence of one or more data nodes. Each **data node** contains data (an int, struct, etc.) and an optional pointer to another data node.
+
+Suppose, for whatever reason, that we want to cyclize a singly linked list into a **ring.** In other words, we want to connect the tail node to the head node. Doing so directly results in an ADT in which the there are no intrinsic notions of beginning and end. If we wanted a reproducible traversal of the ring’s data nodes, then we would have to manage an extra pointer into the ring.
+
+An alternative solution is to come up with the idea of a **gate node,** which consists entirely of one optional pointer to a data node. We then redefine the **ring** as a sequence of one gate node and zero or more data nodes. A ring that has zero data nodes is an **empty ring.** The gate node in an empty ring doesn’t point to any node. In a non-empty ring, the gate node points to the first data node and the last data node points to the gate node. Thus, we have a ring ADT for which traversal is reproducible and managed by the implementation.
+
+To further increase the flexibility of this ADT (again, for whatever reason), we can introduce hierarchical properties by allowing every data node to know about a child gate node and every gate node to know about a parent data node. This new *multiring* ADT allows us to “descend into” and “ascend from” subrings. Traversing a multiring feels like traversing both a cyclic linked list and a [tree].
+
+We stipulate that the first gate node (the **root node**) may not have a parent data node who is also one of its descendents. This restriction prevents the creation of internal traversal loops, which would have the effect of removing arbitrarily many nodes from the multiring.
+
+## Usage
+
+Please see the unit tests in [*multiring.zig*][source]. The `MultiRing` API is still unstable; its basic usage is presently too verbose for high-level documentation. [Ryoko] doesn’t recommend using this module in production.
+
+## Applications
+
+multiring.zig has no known applications. Ryoko wrote it to practice Zig, have fun and show linked lists some love. If you have used multiring.zig successfully in your project(s), please let us know by [starting a discussion][discussions]. (Please take time to read the [code of conduct] before starting a discussion.)
+
+## License
+
+multiring.zig is free and open source software [licensed under the MIT license][license].
+
+## Acknowledgements
+
+The implementation is inspired by the [`std.SinglyLinkedList`][std.SinglyLinkedList] implementation in Zig 0.9.1.
+
+[abstract data type]: https://en.wikipedia.org/wiki/Abstract_data_type
+[code of conduct]: ./CODE_OF_CONDUCT.md
+[discussions]: https://github.com/ok-ryoko/multiring.zig/discussions
+[license]: ./LICENSE.txt
+[linked list]: https://en.wikipedia.org/wiki/Linked_list
+[Ryoko]: https://github.com/ok-ryoko
+[source]: ./src/multiring.zig
+[std.SinglyLinkedList]: https://github.com/ziglang/zig/blob/0.9.1/lib/std/linked_list.zig
+[tree]: https://en.wikipedia.org/wiki/Tree_(data_structure)
+[Zig]: https://ziglang.org/
---
+++ src/multiring.zig
@@ -0,0 +1,353 @@
+const RingError = error{
+    DataNodeAlreadyHasChild,
+    GateNodeAlreadyHasParent,
+    UnsafeLoopCreationAttempt,
+};
+
+/// Singly linked, cyclic and hierarchical abstract data type
+pub fn MultiRing(comptime T: type) type {
+    return struct {
+        const Self = @This();
+
+        pub const Node = union(enum) {
+            gate: *GateNode,
+            data: *DataNode,
+
+            pub fn insertAfter(self: Node, new_node: *DataNode) void {
+                switch (self) {
+                    .gate => |s| {
+                        s.insertAfter(new_node);
+                    },
+                    .data => |s| {
+                        s.insertAfter(new_node);
+                    },
+                }
+            }
+
+            pub fn popNext(self: Node) ?*DataNode {
+                return switch (self) {
+                    .gate => |s| s.popNext(),
+                    .data => |s| s.popNext(),
+                };
+            }
+
+            pub fn step(self: Node) Node {
+                return switch (self) {
+                    .gate => |s| s.step(),
+                    .data => |s| s.step(),
+                };
+            }
+        };
+
+        /// Node that acts as an entry point into a possibly empty ring
+        pub const GateNode = struct {
+            next: ?*DataNode = null,
+            parent: ?*DataNode = null,
+
+            pub fn insertAfter(node: *GateNode, new_node: *DataNode) void {
+                if (node.next) |next_node| {
+                    new_node.next = .{ .data = next_node };
+                } else {
+                    // The gate is the only node in the ring, so connect the
+                    // new node back to the gate
+                    //
+                    new_node.next = .{ .gate = node };
+                }
+                node.next = new_node;
+            }
+
+            pub fn popNext(node: *GateNode) ?*DataNode {
+                const next_node = node.next orelse return null;
+                node.next = switch (next_node.next.?) {
+                    .gate => null,
+                    .data => |next_next_node| next_next_node,
+                };
+                return next_node;
+            }
+
+            /// Step forward by one node, traversing the multiring as if it 
+            /// were a cyclic linked list
+            pub fn step(node: *GateNode) Node {
+                if (node.next) |next_node| {
+                    return .{ .data = next_node };
+                } else {
+                    return .{ .gate = node };
+                }
+            }
+        };
+
+        /// Node that holds data and can be an attachment point for a subring
+        pub const DataNode = struct {
+            next: ?Node = null,
+            child: ?*GateNode = null,
+            data: T,
+
+            pub const Data = T;
+
+            pub fn insertAfter(node: *DataNode, new_node: *DataNode) void {
+                new_node.next = node.next;
+                node.next = .{ .data = new_node };
+            }
+
+            pub fn popNext(node: *DataNode) ?*DataNode {
+                switch (node.next.?) {
+                    .gate => {
+                        return null;
+                    },
+                    .data => |next_node| {
+                        node.next = next_node.next;
+                        return next_node;
+                    },
+                }
+            }
+
+            pub fn step(node: *DataNode) Node {
+                // If this node has a child gate node of a ring containing at
+                // least one data node, then go to the first data node in the
+                // subring; otherwise, go to the next data node at this level
+                //
+                const child = node.child;
+                if (child != null and child.?.next != null) {
+                    return .{ .data = child.?.next.? };
+                } else {
+                    switch (node.next.?) {
+                        .gate => |next_node| {
+                            if (next_node.parent) |parent| {
+                                // The next node is a gate node with a parent data node, so
+                                // go to the next data node in the upper level
+                                //
+                                return parent.next.?;
+                            } else {
+                                // The next node is the last gate node in the multiring, so
+                                // go to the first data node after the root node
+                                //
+                                return .{ .data = next_node.next.? };
+                            }
+                        },
+                        .data => |next_node| {
+                            return .{ .data = next_node };
+                        },
+                    }
+                }
+            }
+
+            pub fn popSubring(node: *DataNode) ?*GateNode {
+                return if (node.child) |gate_node| blk: {
+                    node.child = null;
+                    gate_node.parent = null;
+                    break :blk gate_node;
+                } else null;
+            }
+        };
+
+        root: ?*GateNode = null,
+
+        /// Remove a data node from the ring, returning true if the node was 
+        /// found and removed and otherwise false
+        pub fn remove(ring: *Self, node: *DataNode) bool {
+            if (ring.root.?.next.? == node) {
+                switch (node.next.?) {
+                    .gate => {
+                        ring.root.?.next = null;
+                    },
+                    .data => |next_node| {
+                        ring.root.?.next = next_node;
+                    },
+                }
+                return true;
+            }
+
+            var iterator: Node = .{ .data = ring.root.?.next.? };
+            while (true) {
+                // Once we're in this loop, the iterator will never stop at a
+                // gate node according to the behaviour of Node.step()
+                //
+                switch (iterator) {
+                    .gate => {
+                        unreachable;
+                    },
+                    .data => |it| {
+                        switch (it.next.?) {
+                            .gate => |next_node| {
+                                if (next_node == ring.root.?) {
+                                    return false;
+                                }
+                            },
+                            .data => |next_node| {
+                                if (next_node == node) {
+                                    it.next = node.next;
+                                    next_node.next = null;
+                                    return true;
+                                }
+                            },
+                        }
+                        iterator = iterator.step();
+                    },
+                }
+            }
+        }
+
+        pub fn attachSubring(ring: *Self, node: *DataNode, gate: *GateNode) !void {
+            if (node.child != null) {
+                return RingError.DataNodeAlreadyHasChild;
+            } else if (gate.parent != null) {
+                return RingError.GateNodeAlreadyHasParent;
+            } else if (gate == ring.root) {
+                return RingError.UnsafeLoopCreationAttempt;
+            }
+
+            node.child = gate;
+            gate.parent = node;
+        }
+    };
+}
+
+test "fundamental operations" {
+    const std = @import("std");
+    const testing = std.testing;
+
+    const M = MultiRing(u8);
+    var g0 = M.GateNode{};
+    var multiring = M{ .root = &g0 };
+
+    var r0_data_nodes = [_]M.DataNode{
+        .{ .data = 1 },
+        .{ .data = 7 },
+        .{ .data = 5 },
+        .{ .data = 8 },
+    };
+    g0.insertAfter(&r0_data_nodes[0]);
+    try testing.expectEqual(&r0_data_nodes[0], g0.next.?);
+    try testing.expectEqual(M.Node{ .gate = &g0 }, r0_data_nodes[0].next.?);
+
+    r0_data_nodes[0].insertAfter(&r0_data_nodes[1]);
+    try testing.expectEqual(M.Node{ .data = &r0_data_nodes[1] }, r0_data_nodes[0].next.?);
+    try testing.expectEqual(M.Node{ .gate = &g0 }, r0_data_nodes[1].next.?);
+
+    r0_data_nodes[1].insertAfter(&r0_data_nodes[2]);
+    r0_data_nodes[2].insertAfter(&r0_data_nodes[3]);
+
+    var g1 = M.GateNode{};
+    var r1_data_nodes = [_]M.DataNode{
+        .{ .data = 9 },
+        .{ .data = 0 },
+        .{ .data = 9 },
+        .{ .data = 1 },
+        .{ .data = 2 },
+        .{ .data = 2 },
+    };
+    g1.insertAfter(&r1_data_nodes[0]);
+    r1_data_nodes[0].insertAfter(&r1_data_nodes[1]);
+    r1_data_nodes[1].insertAfter(&r1_data_nodes[2]);
+    r1_data_nodes[2].insertAfter(&r1_data_nodes[3]);
+    r1_data_nodes[3].insertAfter(&r1_data_nodes[4]);
+    r1_data_nodes[4].insertAfter(&r1_data_nodes[5]);
+
+    // attach subring r1 to ring r0
+    try multiring.attachSubring(&r0_data_nodes[0], &g1);
+    try testing.expectEqual(&g1, r0_data_nodes[0].child.?);
+    try testing.expectEqual(&r0_data_nodes[0], g1.parent.?);
+
+    var g2 = M.GateNode{};
+    var r2_data_nodes = [_]M.DataNode{
+        .{ .data = 9 },
+        .{ .data = 8 },
+        .{ .data = 2 },
+        .{ .data = 2 },
+        .{ .data = 7 },
+    };
+    g2.insertAfter(&r2_data_nodes[0]);
+    r2_data_nodes[0].insertAfter(&r2_data_nodes[1]);
+    r2_data_nodes[1].insertAfter(&r2_data_nodes[2]);
+    r2_data_nodes[2].insertAfter(&r2_data_nodes[3]);
+    r2_data_nodes[3].insertAfter(&r2_data_nodes[4]);
+
+    try multiring.attachSubring(&r0_data_nodes[2], &g2);
+
+    var g3 = M.GateNode{};
+    var r3_data_nodes = [_]M.DataNode{
+        .{ .data = 6 },
+        .{ .data = 0 },
+        .{ .data = 4 },
+    };
+    g3.insertAfter(&r3_data_nodes[0]);
+    r3_data_nodes[0].insertAfter(&r3_data_nodes[1]);
+    r3_data_nodes[1].insertAfter(&r3_data_nodes[2]);
+
+    try multiring.attachSubring(&r2_data_nodes[1], &g3);
+
+    var g4 = M.GateNode{};
+    try multiring.attachSubring(&r2_data_nodes[3], &g4);
+
+    // step forward to the first data node
+    try testing.expectEqual(
+        M.Node{ .data = &r0_data_nodes[0] },
+        g0.step(),
+    );
+
+    // descend into subring r1
+    try testing.expectEqual(
+        M.Node{ .data = &r1_data_nodes[0] },
+        r0_data_nodes[0].step(),
+    );
+
+    // ascend from r1
+    try testing.expectEqual(
+        M.Node{ .data = &r0_data_nodes[1] },
+        r1_data_nodes[5].step(),
+    );
+
+    // skip over an empty subring (r4)
+    try testing.expectEqual(
+        M.Node{ .data = &r2_data_nodes[4] },
+        r2_data_nodes[3].step(),
+    );
+
+    // loop back from the last data node to the first data node
+    try testing.expectEqual(
+        M.Node{ .data = &r0_data_nodes[0] },
+        r0_data_nodes[3].step(),
+    );
+
+    // remove a data node in the multiring (in r3)
+    try testing.expect(multiring.remove(&r3_data_nodes[2]));
+    try testing.expectEqual(M.Node{ .gate = &g3 }, r3_data_nodes[1].next.?);
+    try testing.expectEqual(@as(?M.Node, null), r3_data_nodes[2].next);
+
+    // try to remove a data node that isn't in the multiring
+    var d = M.DataNode{ .data = 0 };
+    try testing.expect(!multiring.remove(&d));
+    try testing.expectEqual(@as(?M.Node, null), d.next);
+
+    // pop a data node from a data node in r3
+    try testing.expectEqual(&r3_data_nodes[1], r3_data_nodes[0].popNext().?);
+    try testing.expectEqual(M.Node{ .gate = &g3 }, r3_data_nodes[0].next.?);
+
+    // pop a data node from the r3 gate node
+    try testing.expectEqual(&r3_data_nodes[0], g3.popNext().?);
+    try testing.expectEqual(@as(?*M.DataNode, null), g3.next);
+
+    // try to pop the next data node from g3 (there isn't one)
+    try testing.expectEqual(@as(?*M.DataNode, null), g3.popNext());
+    try testing.expectEqual(@as(?*M.DataNode, null), g3.next);
+
+    // pop r3 (now empty) from r2
+    try testing.expectEqual(&g3, r2_data_nodes[1].popSubring().?);
+    try testing.expectEqual(@as(?*M.GateNode, null), r2_data_nodes[1].child);
+    try testing.expectEqual(@as(?*M.DataNode, null), g3.parent);
+    try testing.expectEqual(M.Node{ .data = &r2_data_nodes[2] }, r2_data_nodes[1].step());
+
+    // try to attach subrings inappropriately
+    try testing.expectError(
+        RingError.DataNodeAlreadyHasChild,
+        multiring.attachSubring(&r0_data_nodes[0], &g3),
+    );
+    try testing.expectError(
+        RingError.GateNodeAlreadyHasParent,
+        multiring.attachSubring(&r1_data_nodes[0], &g2),
+    );
+    try testing.expectError(
+        RingError.UnsafeLoopCreationAttempt,
+        multiring.attachSubring(&r1_data_nodes[0], &g0),
+    );
+}
