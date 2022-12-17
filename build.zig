16b7bb013ed4486f50f18ebc2d5e4d450ae7d3e2
OK Ryoko <ryoko@kyomu.jp.net>
2022-12-16 21:09:21 -0500

    reset build script

--- build.zig
+++ build.zig
@@ -1,13 +1,15 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
-    const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
-    const exe_tests = b.addTest("src/multiring.zig");
-    exe_tests.setTarget(target);
-    exe_tests.setBuildMode(mode);
+    const lib = b.addStaticLibrary("tmp", "src/multiring.zig");
+    lib.setBuildMode(mode);
+    lib.install();
 
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&exe_tests.step);
+    const main_tests = b.addTest("src/multiring.zig");
+    main_tests.setBuildMode(mode);
+
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&main_tests.step);
 }
