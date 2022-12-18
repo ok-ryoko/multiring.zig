d3fb4647d02f48599322f4c2e5403aed3db14b17
OK Ryoko <ryoko@kyomu.jp.net>
2022-12-18 16:50:46 -0500

    update build script
    - add support for choosing a target at the command line
    - give the library a proper name
    - rename `main_tests` to the simpler `tests`

--- build.zig
+++ build.zig
@@ -2,14 +2,16 @@ const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
     const mode = b.standardReleaseOptions();
+    const target = b.standardTargetOptions(.{});
 
-    const lib = b.addStaticLibrary("tmp", "src/multiring.zig");
+    const lib = b.addStaticLibrary("multiring", "src/multiring.zig");
     lib.setBuildMode(mode);
+    lib.setTarget(target);
     lib.install();
 
-    const main_tests = b.addTest("src/multiring.zig");
-    main_tests.setBuildMode(mode);
+    const tests = b.addTest("src/multiring.zig");
+    tests.setBuildMode(mode);
 
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    test_step.dependOn(&tests.step);
 }
